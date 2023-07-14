38615e502fad3d1c7246e73ad016700a9f474cae
JunkGeniesLLC <139576566+JunkGeniesLLC@users.noreply.github.com>
2023-07-14 15:08:38 -0400

    Styling for first pass complete

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  
-  // Open the "About Us" tab by default
-  document.getElementById("about").style.display = "block";
-  
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+  if (tabName === "choose") {
+    document.body.style.backgroundImage = "url(stockjunk2.jpg)";
+  } else if (tabName === "contact") {
+    document.body.style.backgroundImage = "url(genie_small.jpg)";
+  } else {
+    document.body.style.backgroundImage = "url(stockjunk1.jpg)";
+  }
+}
+
+// Open the "About Us" tab by default
+document.getElementById("about").style.display = "block";
